c2423c792ee0c7a11b929d49daa66fa9229311e7
Nick Zhao <nick.zhao@mail.utoronto.ca>
2022-12-12 16:50:46 -0500

    Update correlation analysis
    add both mean and median stats.

--- Code/Data Extraction/correlation_analysis.py
+++ Code/Data Extraction/correlation_analysis.py
@@ -10,13 +10,19 @@ grassp_mean = data['Mean score']
 grassp_median = data['Median score']
 uems = data['UEMS Score']
 
-pearson = scipy.stats.pearsonr(grassp_mean, uems, alternative='greater')
-spearman = scipy.stats.spearmanr(grassp_mean, uems, alternative='greater')
+mean_pearson = scipy.stats.pearsonr(grassp_mean, uems, alternative='greater')
+mean_spearman = scipy.stats.spearmanr(grassp_mean, uems, alternative='greater')
+median_pearson = scipy.stats.pearsonr(grassp_median, uems, alternative='greater')
+median_spearman = scipy.stats.spearmanr(grassp_median, uems, alternative='greater')
 
 shapiro_mean = scipy.stats.shapiro(grassp_mean)
+shapiro_median = scipy.stats.shapiro(grassp_median)
 shapiro_uems = scipy.stats.shapiro(uems)
 
-print(f'Spearman correlation coefficient: {spearman.correlation}; pvalue: {spearman.pvalue}')
-print(f'Pearson correlation coefficient: {pearson.statistic}; pvalue: {pearson.pvalue}')
+print(f'Spearman correlation coefficient(mean): {mean_spearman.correlation}; pvalue: {mean_spearman.pvalue}')
+print(f'Pearson correlation coefficient(mean): {mean_pearson.statistic}; pvalue: {mean_pearson.pvalue}')
+print(f'Spearman correlation coefficient(median): {median_spearman.correlation}; pvalue: {median_spearman.pvalue}')
+print(f'Pearson correlation coefficient(median): {median_pearson.statistic}; pvalue: {median_pearson.pvalue}')
 print(f'GRASSP mean score Shapiro-Wilk score: {shapiro_mean.statistic}; pvalue: {shapiro_mean.pvalue}')
+print(f'GRASSP mean score Shapiro-Wilk score: {shapiro_median.statistic}; pvalue: {shapiro_median.pvalue}')
 print(f'UEMS Shapiro-Wilk score: {shapiro_uems.statistic}; pvalue: {shapiro_uems.pvalue}')
