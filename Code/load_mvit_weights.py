d42347c8ffe35a7142dbbb8c5c44e5e47cbdcab2
Nick Zhao <nick.zhao@mail.utoronto.ca>
2022-12-19 16:25:25 -0500

    Add SSv2-pretrained mvit_v2 support
    Add support for mvit_v2_b, pretrained with SSv2.

--- Code/load_mvit_weights.py
+++ Code/load_mvit_weights.py
@@ -1,5 +1,6 @@
 import torch
 from torchvision.models.video.mvit import mvit_v2_s
+from .lightning_classes.mvit import build_mvit_v2_b, build_mvit_v2_s
 
 def change_key(self, old, new):
     for _ in range(len(self)):
@@ -13,7 +14,7 @@ def load_mvit_weights(  weight_path: str,
     opt_state = f['optimizer_state']
     cfg = f['cfg']
 
-    model = mvit_v2_s()
+    model = build_mvit_v2_b()
     old_keys = list(state_dict.keys())
     for key in old_keys:
         new_key = ''
@@ -22,6 +23,7 @@ def load_mvit_weights(  weight_path: str,
         if key == 'cls_token': new_key = 'pos_encoding.class_token'
         elif 'patch_embed' in key: new_key = key.replace('patch_embed.proj', 'conv_proj')
         elif 'head.projection' in key: new_key = key.replace('head.projection', 'head.1')
+        elif 'head.projectection' in key: new_key = key.replace('head.projectection', 'head.1')
         elif 'attn.proj' in key: new_key = key.replace('attn.proj', 'attn.project.0')
         elif 'attn.pool_q' in key: new_key = key.replace('attn.pool_q', 'attn.pool_q.pool')
         elif 'attn.norm_q' in key: new_key = key.replace('attn.norm_q', 'attn.pool_q.norm_act.0')
@@ -44,4 +46,4 @@ def load_mvit_weights(  weight_path: str,
     state_dict.pop('head.1.weight', None)
     state_dict.pop('head.1.bias', None)
     model.load_state_dict(state_dict, strict=False)
-    torch.save(model.state_dict(), '../Models/mvit_SSv2/mvitv2_s_ssv2_new.pyth')
+    torch.save(model.state_dict(), './Models/mvit_SSv2/mvitv2_b_ssv2_new.pyth')
